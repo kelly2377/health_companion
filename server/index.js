0e45c191c853f40bd519c8abfa06e22acc13c0d3
Kelly <kelly.garcia.2377@gmail.com>
2026-02-28 15:25:18 -0500

    added data files in files directory

--- server/index.js
+++ server/index.js
@@ -13,3 +13,30 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
+
+  app.post("/analyze-labs", async (req, res) => {
+    const labs = req.body.labs
+  
+    const referenceRanges = {
+      glucose: { min: 70, max: 99, unit: "mg/dL" },
+      iron: { min: 60, max: 170, unit: "mcg/dL" }
+    }
+  
+    const analyzed = labs.map(lab => {
+      const ref = referenceRanges[lab.testType]
+      let status = "normal"
+  
+      if (lab.value < ref.min) status = "low"
+      if (lab.value > ref.max) status = "high"
+  
+      return {
+        ...lab,
+        idealRange: `${ref.min}-${ref.max} ${ref.unit}`,
+        status
+      }
+    })
+  
+    res.json({ analyzed })
+  })
+
+  
